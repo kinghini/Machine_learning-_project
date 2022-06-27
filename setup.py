546ae77733ecb24f20f5e735d46054de2ad48527
Nimmymathew <nimmymaths91@gmail.com>
2022-06-28 04:57:12 +0530

    updating docker file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools  import setup
+from setuptools  import setup,find_packages
 from typing import List
 
 
@@ -8,12 +8,12 @@ PROJECT_NAME="housing-predictor",
 VERSION="0.0.1"
 AUTHOR="NimmyMathew"
 DESCRIPTION="This is first fsds machine learning project housing "
-PACKAGES=["housing"]
+#PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e")
 
 
 
@@ -23,11 +23,10 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 
 )
 
-if __name__=="__main__":
-    print("hhghgg")
-    
+
+
